daf43e420a271eb05b7392e524579c8ae616c6e8
Suketu7879 <suketurola4@gmail.com>
2025-02-26 17:03:33 +0530

    URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://task-manager-api-2loc.onrender.com/api";
+const API_URL = "https://task-manager-zcy8.onrender.com/api";
 
 export const register = (userData) =>
   axios.post(`${API_URL}/auth/register`, userData);
--- server/index.js
+++ server/index.js
@@ -9,28 +9,14 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.options("*", cors());
-// app.use(
-//   cors({
-//     origin: "https://task-manager-chi-coral.vercel.app",
-//     methods: "GET,POST,PUT,DELETE",
-//     allowedHeaders: "Content-Type,Authorization",
-//   })
-// );
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://task-manager-chi-coral.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
+app.use(
+  cors({
+    origin: "https://task-manager-chi-coral.vercel.app",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URI)
