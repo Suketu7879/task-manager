7fcb30de92c770f5ac8a494612cfeb909510ab55
Suketu7879 <suketurola4@gmail.com>
2025-02-26 16:32:47 +0530

    acao

--- server/index.js
+++ server/index.js
@@ -9,13 +9,28 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.options("*", cors());
-app.use(
-  cors({
-    origin: "https://task-manager-chi-coral.vercel.app",
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://task-manager-chi-coral.vercel.app",
+//     methods: "GET,POST,PUT,DELETE",
+//     allowedHeaders: "Content-Type,Authorization",
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://task-manager-chi-coral.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
